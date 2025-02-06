9df9790df6c5b483ae0435e95bdcb4eb8538bd5b
Casey Crosby <caseycrozby@gmail.com>
2025-02-06 14:35:31 -0500

    update regex

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ const results = document.getElementById("results-div");
 
 const useRegex = (input) => {
     const userInput = input;
-    let regex = /1*([0-9][0-9][0-9])+*([0-9][0-9][0-9])+*([0-9][0-9][0-9][0-9])/;
+    let regex = /1*([0-9]+([0-9]+)+)/i;
     return regex.test(input);
 }
 
 
 
 const checkFunction = () => {
-   if (userInput.value === "") {
+   if (userInput.value == "") {
     alert("Please provide a phone number");
   } else if (useRegex(userInput.value)) {
     results.textContent = `Valid US number: ${userInput.value}`
