cb15c69db40bebc0378b7d5489663cbf28cd498f
Casey Crosby <caseycrozby@gmail.com>
2025-02-06 17:16:35 -0500

    main function change

--- script.js
+++ script.js
@@ -3,22 +3,36 @@ const checkBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
 const results = document.getElementById("results-div");
 
-const useRegex = (input) => {
-    const userInput = input;
-    let regex = /1*([0-9]+([0-9]+)+)/i;
-    return regex.test(input);
+
+/****************************************************
+const useValidRegex = () => {
+    let regex = /(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})/;
+    return regex.test(userInput.value);
+}
+*****************************************************/
+const input = userInput.value
+
+const userFunction = (input) => {
+    const regex = /[\D_]/g;
+    const newInput = input.replace(regex, '');
+    return newInput;
+}
+
+const validRegex = () => {
+    const regex = /[1]*[/d]{3}[\d]{3}[\d]{4}/;
+    return regex.test(userFunction);
 }
 
 
 
-const checkFunction = () => {
-   if (userInput.value == "") {
+const checkFunction = (input) => {
+   if (input === "") {
     alert("Please provide a phone number");
-  } else if (useRegex(userInput.value)) {
+  } else if (validRegex() == true) {
     results.textContent = `Valid US number: ${userInput.value}`
   } else {
     return results.textContent = `Invalid US number: ${userInput.value}`;
-  }
+  } 
 };
 
 const clearFunction = () => {
@@ -34,4 +48,6 @@ checkBtn.addEventListener("click", checkFunction);
 
 clearBtn.addEventListener("click", clearFunction);
 
-console.log(useRegex(userInput.value));
+console.log(userFunction(input));
+console.log(validRegex())
+// 1?\W*([2-9][0-8][0-9])\W*([2-9][0-9]{2})\W*([0-9]{4})(\se?x?t?(\d*))?
